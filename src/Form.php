5090ab80136db06961520702d9edee4108618957
Jascha Gerles <Jascha Gerles>
2016-11-20 22:01:25 +0100

    init

---
+++ src/Form.php
@@ -0,0 +1,276 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jascha
+ * Date: 20.09.2016
+ * Time: 05:16
+ */
+class Form {
+
+    /**
+     * Erstellt einen <form>-Tag inklusive cfrs-Token
+     * @param string $route
+     * @param array $settings
+     * @return string
+     */
+    public static function open($action = '', $settings = []) {
+        /*
+         * Ersetzt nicht angegebene Einstellungen durch die Standards
+         */
+        $defaults = [
+            'method' => 'POST',
+            'file' => false,
+            'class' => null,
+            'accept-charset' => 'UTF-8',
+        ];
+        $settings = array_merge($defaults, $settings);
+
+        /*
+         * Erstellt die Attribute
+         */
+        $settings['action'] = $action;
+        if ($settings['file'] == true)
+            $settings['enctype'] = 'multipart/form-data';
+        unset($settings['file']);
+        /*
+         * Erstellt einen Tag und gibt ihn zurück
+         */
+        return Html::createTag('form', $settings) . csrf_field();
+    }
+
+    /**
+     * Schließt die Form
+     * @return string
+     */
+    public static function close() {
+        return '</form>';
+    }
+
+    /**
+     * Erstellt ein Label
+     * @param string $name
+     * @param string $label
+     * @return string
+     */
+    public static function label($name, $label) {
+        return Html::createTag('label', ['for' => $name], $label);
+    }
+
+    /**
+     * Erstellt ein Textfeld mit Label und Wrapper
+     * @param string $name
+     * @param string $label
+     * @param string $input
+     * @return string
+     */
+    public static function text($name, $label = null, $input = '', $attributes = []) {
+        if ($label != null) {
+            $label = Form::label($name, $label);
+        }
+        $input = Form::textInput($name, $input, $attributes);
+
+        return Html::createTag('div', ['class' => 'input-text'],
+            $label . $input
+        );
+
+    }
+
+    /**
+     * Erstellt einen Text-Input mit altem Wert
+     * @param string $name
+     * @param string $input
+     * @param array $attributes
+     * @return string
+     */
+    public static function textInput($name, $input = '', $attributes = []) {
+        $value = function_exists('old') && strlen(old($name)) > 0 ? old($name) : $input;
+        return Html::createTag('input', array_merge(
+            ['type' => 'text', 'name' => $name, 'value' => $value],
+            $attributes
+        ));
+    }
+
+    /**
+     * Erstellt eine Textarea mit Label und Input
+     *
+     * @param $name
+     * @param string|null $label
+     * @param string $input
+     * @return string
+     */
+    public static function textarea($name, $label = null, $input = '', $attributes = []) {
+        if ($label != null) {
+            $label = Form::label($name, $label);
+        }
+        $input = Form::textareaInput($name, $input, $attributes);
+
+        return Html::createTag('div', ['class' => 'input-textarea'],
+            $label . $input
+        );
+    }
+
+    /**
+     * Erstellt eine Textarea
+     *
+     * @param string $name
+     * @param string $input
+     * @param array $attributes
+     * @return string
+     */
+    public static function textareaInput($name, $input = '', $attributes = []) {
+        $attributes['name'] = $name;
+        return Html::createTag('textarea', $attributes, $input, true);
+    }
+
+
+    /**
+     * Erstellt ein E-Mail-Feld mit Label und Wrapper
+     * @param string $name
+     * @param string $label
+     * @return string
+     */
+    public static function email($name, $label) {
+        $label = Form::label($name, $label);
+        $input = Form::emailInput($name);
+
+        return Html::createTag('div', ['class' => 'input-text'],
+            $label . $input
+        );
+
+    }
+
+    /**
+     * Erstellt einen E-Mail-Input mit altem Wert
+     * @param string $name
+     * @return string
+     */
+    public static function emailInput($name) {
+        $old = function_exists('old') ? old($name) : '';
+        return Html::createTag('input', ['type' => 'email', 'name' => $name, 'value' => $old]);
+    }
+
+    /**
+     * Erstellt ein Passwort-Feld mit Label und Wrapper
+     * @param string $name
+     * @param string $label
+     * @return string
+     */
+    public static function password($name, $label, $attributes = []) {
+        $label = Form::label($name, $label);
+        $input = Form::passwordInput($name, $attributes);
+
+        return Html::createTag('div', ['class' => 'input-text'],
+            $label . $input
+        );
+    }
+
+    /**
+     * Erstellt einen Passwort-Input
+     * @param string $name
+     * @return string
+     */
+    public static function passwordInput($name = 'password', $attributes = []) {
+        $attributes['type'] = 'password';
+        $attributes['name'] = $name;
+        return Html::createTag('input', $attributes);
+    }
+
+    /**
+     * Erstellt einen Datei-Input
+     * @param string $name
+     * @param string $label
+     * @return string
+     */
+    public static function file($name, $label, $attributes = []) {
+        $attributes['type'] = 'file';
+        $attributes['name'] = $name;
+
+        $label = Form::label($name, $label);
+        $input = Html::createTag('input', $attributes);
+        return Html::createTag('div', ['class' => 'input-file'], $label . $input);
+    }
+
+    /**
+     * Erstellt ein hidden-Field
+     * @param string $name
+     * @param string $value
+     * @param array $attributes
+     * @return string
+     */
+    public static function hidden($name, $value = null, $attributes = []) {
+        $attributes['type'] = 'hidden';
+        $attributes['name'] = $name;
+        $attributes['value'] = $value;
+        return Html::createTag('input', $attributes);
+    }
+
+    /**
+     * @param string $name
+     * @param array $options
+     * @param null $default
+     * @param array $attributes
+     * @return string
+     */
+    public static function select($name, $options = [], $default = null, $attributes = []) {
+        $attributes['name'] = $name;
+        $opts = '';
+        foreach ($options as $value => $name) {
+            if($value === $default) {
+                $opts .= Html::createTag('option', ['value' => $value, 'selected'], $name, true);
+            } else {
+                $opts .= Html::createTag('option', ['value' => $value], $name, true);
+            }
+        }
+        return Html::createTag('select', $attributes, $opts, true);
+    }
+
+    public static function radio($name, $value, $description, $checked = false, $attributes = []) {
+        $attributes['type'] = 'radio';
+        $attributes['name'] = $name;
+        $attributes['value'] = $value;
+        if ($checked) {
+            $attributes[] = 'checked';
+        }
+        return Html::createTag('label', [],
+            Html::createTag('input', $attributes) . ' ' . $description
+        );
+    }
+
+    public static function checkbox($name, $label, $value = 1, $checked = 0) {
+        $attributes = [
+            'type' => 'checkbox',
+            'name' => $name,
+            'value' => $value
+        ];
+        if($checked != 0 && $checked != null) {
+            $attributes[] = 'checked';
+        }
+        $input = Html::createTag('input', $attributes);
+        return Html::createTag('label', [], $input . ' ' . $label, true);
+    }
+
+    /**
+     * Erstellt einen Button inkl Wrapper
+     * @param string $text
+     * @param string $class
+     * @return string
+     */
+    public static function submit($text, $class = null) {
+        return Html::createTag('div', ['class' => 'input-submit',],
+            Form::submitButton($text, $class)
+        );
+    }
+
+    /**
+     * Erstellt eine Button
+     * @param string $text
+     * @param string $class
+     * @return string
+     */
+    public static function submitButton($text, $class) {
+        $class = $class != null ? 'btn btn-' . $class : 'btn';
+        return Html::createTag('button', ['type' => 'submit', 'class' => $class], $text);
+    }
+
+}
---
+++ src/Html.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jasch
+ * Date: 20.09.2016
+ * Time: 06:25
+ */
+class Html {
+    private $tag;
+    private $attributes;
+    private $inside;
+
+    public function __construct($tag, $attributes = [], $inside = null, $alwaysClose = false) {
+        $this->tag = $tag;
+        $this->attributes = $attributes;
+        $this->inside = $inside;
+        $this->alwaysClose = $alwaysClose;
+    }
+
+    /**
+     * Erstellt ein Objekt und gibt es als String aus.
+     * Wird ein $inside angegeben, wird dieses eingefügt; gefolgt vom closing Tag
+     *
+     * @param string $tag String des Tags
+     * @param array $attributes Array mit Attributen
+     * @param string $inside Inneres Element
+     * @param bool $alwaysClose erstellt auch ohne $inside einen closing Tag
+     * @return string
+     */
+    public static function createTag($tag, $attributes = [], $inside = null, $alwaysClose = false) {
+        $html = new Html($tag, $attributes, $inside, $alwaysClose);
+        if($html->inside == null && $html->alwaysClose === false) {
+            return $html->toString();
+        }
+
+        return $html->toString() . $html->inside . $html->closingTag();
+
+    }
+
+    /**
+     * Erstellt einen String aus dem Objekt
+     * @return string
+     */
+    public function toString() {
+        return '<' . $this->tag . $this->attributesToString() . '>';
+    }
+
+    /**
+     * Erstellt einen String mit den Attributen eines Tags
+     * @param array $attributes
+     * @return string
+     */
+    private function attributesToString() {
+        $output = '';
+        foreach($this->attributes as $key => $val) {
+            $output .= $this->attributeToString($key, $val);
+        }
+        return $output;
+    }
+
+    /**
+     * Erstellt ein Attribut aus Bezeichnung und Wert mit vorangehendem Leerzeichen
+     * @param $key
+     * @param $val
+     * @return string
+     */
+    private function attributeToString($key, $val) {
+        if($val === NULL || $val === FALSE)
+            return '';
+        if(is_string($key)) {
+            return ' ' . $key . '="' . $val . '"';
+        } else {
+            return ' ' . $val;
+        }
+    }
+
+    /**
+     * Erstellt schließenden Tag
+     * @return string
+     */
+    private function closingTag() {
+        return '</' . $this->tag . '>';
+    }
+}
